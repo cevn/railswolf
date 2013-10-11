713dec2abd7d7edab725e92f3f98078cf6fb9fa8
Sameer Dhar <avielus@gmail.com>
2013-10-11 07:39:10 -0400

    fixing postrequest.sh, linebreaks break bash one-liners, who knew

--- scripts/postrequest.sh
+++ scripts/postrequest.sh
@@ -1,4 +1,1 @@
-curl -v -H "Accept: application/json" -H "Content-type: application/json"
--X POST -d "{\"user\": {\"name\":\"Sameer
-Dhar\",\"email\":\"avielus2@gmail.com\",\"password\":\"asdfasdf\",\"password_confirmation\":\"asdfasdf\"}}"
-https://railswolf.herokuapp.com/users -o response.json
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d "{\"user\": {\"name\":\"Sameer Dhar\",\"email\":\"avielus2@gmail.com\",\"password\":\"asdfasdf\",\"password_confirmation\":\"asdfasdf\"}}" https://railswolf.herokuapp.com/users -o response.json
