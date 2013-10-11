4896d1e5351a326363dbd7d83cb76c5012ad51c3
Sameer Dhar <avielus@gmail.com>
2013-10-11 18:54:42 -0400

    Cleaning active admin shit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011175850) do
+ActiveRecord::Schema.define(version: 20131011224328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20131011175850) do
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "name"
+    t.boolean  "admin",                  default: false
     t.integer  "game_id"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
