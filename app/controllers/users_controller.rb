02cf1de957a965011322b1da96470c9de626a811
Sameer Dhar <avielus@gmail.com>
2013-12-16 12:54:49 -0500

    asdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -82,6 +82,7 @@ class UsersController < ApplicationController
     end
 
     def correct_user
+      puts current_user
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
     end
