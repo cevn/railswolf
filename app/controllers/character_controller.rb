31b2854fcad2ae080271c605f2415f8bfe80b6eb
Sameer Dhar <avielus@gmail.com>
2013-12-21 23:23:40 -0500

    asdf

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -42,28 +42,38 @@ class CharactersController < ApplicationController
     respond_with(@living_chars)
   end
 
+  def hi_scores
+    chars = Character.all 
+    respond_with(chars)
+  end
+
+  def scores 
+    chars = Character.all
+    respond_with(chars)
+  end
+
   def vote 
-    @game = Game.find_by_id(1)
-    @voted = Character.find(params[:victimid])
-    @char = Character.find(params[:user_id]) 
+    game = Game.find_by_id(1)
+    voted = Character.find(params[:victimid])
+    char = Character.find(params[:user_id]) 
 
 
-    if @game and @game.night
-      respond_with(@voted) do |format| 
+    if game and game.night
+      respond_with(voted) do |format| 
         format.json {render :json => { :success => :false, :error => "You can only vote during the day!" } }
       end
     else 
       if !@char.voted
-        @voted.votes += 1
-        @voted.save
-        @char.voted = true 
-        @char.score += 200
-        @char.save
-        respond_with(@voted) do |format| 
+        voted.votes += 1
+        voted.save
+        char.voted = true 
+        char.score += 200
+        char.save
+        respond_with(voted) do |format| 
           format.json {render :json => { :success => true } } 
         end
       else 
-        respond_with(@voted) do |format| 
+        respond_with(voted) do |format| 
           format.json {render :json => { :success => :false, :error => "You can only vote once per day." } }
         end
       end
@@ -71,16 +81,16 @@ class CharactersController < ApplicationController
   end
 
   def move
-    @char = Character.find(params[:id]) 
+    char = Character.find(params[:id]) 
 
-    if @char.update_attributes(move_params)
-      @char.score += 10
-      @char.save
-      respond_with(@char) do |format| 
+    if char.update_attributes(move_params)
+      char.score += 10
+      char.save
+      respond_with(char) do |format| 
         format.json {render :json => { :success => true }} 
       end
     else 
-      respond_with(@char) do |format| 
+      respond_with(char) do |format| 
         format.json {render :json => { :success => false }}
       end
     end
