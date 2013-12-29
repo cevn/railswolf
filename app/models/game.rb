8c65d40aef2ff217ef6b1c2276a241d440a594de
Sameer Dhar <avielus@gmail.com>
2013-12-29 05:01:12 -0500

    fixing update task

--- app/models/game.rb
+++ app/models/game.rb
@@ -28,7 +28,7 @@ class Game < ActiveRecord::Base
 
         ## Add event to record 
         event = Event.new 
-        event.victimid = charToKill.id
+        event.victim = charToKill.user.name
         event.killerid = 0 
         event.latitude = charToKill.latitude 
         event.longitude= charToKill.longitude
